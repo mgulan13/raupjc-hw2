0c8de7f843d5a06b7cafa1f23e3a5302f7ea61e2
Marko Gulan <marko.gulan13@gmail.com>
2017-11-05 21:29:23 +0100

    Riješen 6. i 7. zadatak

---
+++ Primjer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zadatak6;
+
+namespace Primjer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Main method is the only method that
+            // can ’t be marked with async .
+            // What we are doing here is just a way for us to simulate
+            // async - friendly environment you usually have with
+            // other .NET application types ( like web apps , win apps etc .)
+            // Ignore main method , you can just focus on LetsSayUserClickedAButtonOnGuiMethod() as a
+            // first method in call hierarchy .
+            var t = Task.Run(() => Factorials.LetsSayUserClickedAButtonOnGuiMethod());
+            Console.Read();
+        }
+    }
+}
---
+++ Zadatak6/Factorials.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak6
+{
+    public class Factorials
+    {
+        public static async void LetsSayUserClickedAButtonOnGuiMethod()
+        {
+            var result = await GetTheMagicNumber();
+            Console.WriteLine(result);
+        }
+        private static async Task<int> GetTheMagicNumber()
+        {
+            return await IKnowIGuyWhoKnowsAGuy();
+        }
+        private static async Task<int> IKnowIGuyWhoKnowsAGuy()
+        {
+            return await IKnowWhoKnowsThis(10) + await IKnowWhoKnowsThis(5);
+        }
+        private static async Task<int> IKnowWhoKnowsThis(int n)
+        {
+            return await FactorialDigitSum(n);
+        }
+
+        public static async Task<int> FactorialDigitSum(int number)
+        {
+            return await Task.Run(() => Factorial(number).ToString().Sum(c => c - '0'));
+        }
+
+        static int Factorial(int number)
+        {
+            int result = number;
+            for (int i = 1; i < number; i++)
+                result = result * i;
+            return result;
+        }
+    }
+}
