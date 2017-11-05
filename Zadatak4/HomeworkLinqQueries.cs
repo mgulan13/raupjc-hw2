d488e49f5488ac83561123d59bc88cc28e3300f6
Marko Gulan <marko.gulan13@gmail.com>
2017-11-05 20:00:26 +0100

    Riješen 4. zadatak

---
+++ Zadatak4/HomeworkLinqQueries.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using Zadatak1;
+
+namespace Zadatak4
+{
+    public class HomeworkLinqQueries
+    {
+        public static string[] Linq1(int[] intArray)
+        {
+            return intArray.GroupBy(x => x)
+                .OrderBy(i => i.Key)
+                .Select(i => $"Broj {i.Key} ponavlja se {i.Count()} puta")
+                .ToArray();
+        }
+
+        public static University[] Linq2_1(University[] universityArray)
+        {
+            return universityArray.Where(i => i.Students.All(x => x.Gender == Gender.Male))
+                .ToArray();
+        }
+
+        public static University[] Linq2_2(University[] universityArray)
+        {
+            double average = universityArray.Select(u => u.Students.Count()).Average();
+            return universityArray.Where(u => u.Students.Count() < average).ToArray();
+        }
+
+        public static Student[] Linq2_3(University[] universityArray)
+        {
+            return universityArray.SelectMany(i => i.Students).Distinct().ToArray();
+        }
+
+        public static Student[] Linq2_4(University[] universityArray)
+        {
+            return universityArray.Where(i => i.Students.All(x => x.Gender == Gender.Male) || i.Students.All(x => x.Gender == Gender.Female))
+                .SelectMany(s => s.Students)
+                .Distinct()
+                .ToArray();
+        }
+        public static Student[] Linq2_5(University[] universityArray)
+        {
+            return universityArray.SelectMany(i => i.Students)
+                .GroupBy(i => i)
+                .Where(i => i.Count() > 1)
+                .Select(i => i.Key)
+                .ToArray();
+        }
+    }
+}
